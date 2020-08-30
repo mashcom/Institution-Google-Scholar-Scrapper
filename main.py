4b39cbe528c96a5c517f2f427d0af0ee9551ecc5
Blessing Mashoko <bmashcom@hotmail.com>
2020-08-30 21:04:05 +0200

    added progress bars and saving or result to csv

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 from prettytable import PrettyTable
 from src.ScholarlyInstitution import ScholarlyInstitution
 from progress.spinner import Spinner
+from scholarly import scholarly
+import csv
 
 if __name__ == "__main__":
 
-    institution_name = input("Type the Institution Name? ")
-    print("**************************************************************")
-    print("|    Institution Google Scholar Scrapper by Blessing Mashoko |\n".upper())
-    print("|    This process my take long, so grab a cup of coffee :-)  |")
-    print("|    Institution: " + institution_name + "                      |")
-    print("**************************************************************")
+    institution_name = "Midlands State University" #input("Type the Institution Name? ")
+    print("+----------------------------------------------------------------------------------------------------+")
+    print("|    Institution Google Scholar Scrapper by Blessing Mashoko                                         |\n".upper())
+    print("|    This process my take long, so grab a cup of coffee :-)                                          |")
+    print("|    Institution: " + institution_name.upper() + "                                                   |")
+    print("+----------------------------------------------------------------------------------------------------+\n\n")
 
     scholarlyInstitution = ScholarlyInstitution(institution_name)
     link = scholarlyInstitution.get_institution_link()
     if link is None:
         print("Institution not found")
         exit()
-    print(link)
-    affiliates = scholarlyInstitution.get_institution_affiliates(link)
+    print("Found: " + link)
 
-    table = PrettyTable(['Id', 'Name', 'Interests'])
+    csv_table_header = ['Id', 'Name', 'Interests']
+    csv_body = []
+    table = PrettyTable(csv_table_header)
     table.align = "l"
+    affiliates = scholarlyInstitution.get_institution_affiliates(link)
     for affiliate in affiliates:
         affiliate_id = affiliate.id
         author_name = affiliate.name
         author_interests = ','.join(affiliate.interests)
-        table.add_row([affiliate_id, author_name, author_interests])
+        processed_affiliate_row = [affiliate_id, author_name, author_interests]
+        csv_body.append(processed_affiliate_row)
+        table.add_row(processed_affiliate_row)
     print(table)
+
+    filename = "output/"+ institution_name+" Affiliates.csv"
+
+    # writing to csv file  
+    with open(filename, 'w', newline="") as csvfile:
+        # creating a csv writer object  
+        csvwriter = csv.writer(csvfile)
+
+        # writing the fields  
+        csvwriter.writerow(csv_table_header)
+
+        # writing the data rows  
+        csvwriter.writerows(csv_body)
--- src/ScholarlyInstitution.py
+++ src/ScholarlyInstitution.py
@@ -1,14 +1,13 @@
-from http.client import RemoteDisconnected
-
 from scholarly import scholarly
 from bs4 import BeautifulSoup
 import requests
-
+from tqdm import trange
+import sys
+import time
 
 class ScholarlyInstitution:
     INSTITUTION_BASE_URL = "https://scholar.google.com/scholar?q="
     institution_name = None
-    institution_id = None
 
     def __init__(self, institution_name):
         if institution_name is not None:
@@ -17,16 +16,16 @@ class ScholarlyInstitution:
             exit('Please provide the instition name')
 
     def get_institution_link(self):
+        for i in trange(10, file=sys.stdout, desc='Getting Parent Link'):
+            url = self.INSTITUTION_BASE_URL + self.institution_name
+            page = requests.get(url)
+            institution_link = None
+            if page.status_code >= 200 or page.status_code <= 299:
+                soup = BeautifulSoup(page.text, 'html.parser')
+                institution = soup.find_all("div", class_="gs_ob_inst_r")
 
-        url = self.INSTITUTION_BASE_URL + self.institution_name
-        page = requests.get(url)
-        institution_link = None
-        if page.status_code >= 200 or page.status_code <= 299:
-            soup = BeautifulSoup(page.text, 'html.parser')
-            institution = soup.find_all("div", class_="gs_ob_inst_r")
-
-            for div in institution:
-                institution_link = div.find('a')['href']
+                for div in institution:
+                    institution_link = div.find('a')['href']
         return institution_link
 
     def get_url_param(self, url, parameter_name):
@@ -36,52 +35,42 @@ class ScholarlyInstitution:
         parameter_found = params[parameter_name]
         return parameter_found
 
-    def get_all_institution_links(self, link):
+    def get_institution_affiliates(self, link):
         if link is None:
             return "No link found"
 
         url = "https://scholar.google.com" + link
-        self.institution_id = self.get_url_param(url, 'org')
-        links = [url]
-        can_progress = True
-        print("Start Link => " + url)
-        while can_progress:
-            page = requests.get(url)
-            soup = BeautifulSoup(page.text, 'html.parser')
-            try:
-                next_page = soup.find("button", class_="gsc_pgn_pnx")['onclick']
-                links.append(""+next_page+"")
-            except RemoteDisconnected:
-                print("Remote end closed connection without response")
-            except:
-                can_progress = False
-                print("The are no more records to get")
-
-        return links
-
-    def get_institution_affiliates(self, link):
-        pages = self.get_all_institution_links(link)
-        print(pages)
-        i = 1
+        organisation_id = self.get_url_param(url, 'org')
         affiliates = []
-        for url in pages:
-            page = requests.get(url)
-            soup = BeautifulSoup(page.text, 'html.parser')
-            profiles = soup.find_all("h3", class_="gs_ai_name")
+        can_progress = True
+        i = 1
+        #print("Start Link => " + url)
 
-            split_next_page_link = page.split('\\')
-            target_start_author_raw = split_next_page_link[len(split_next_page_link) - 3]
-            after_author_param = target_start_author_raw.replace('x3d', '')
-            astart = i * 10
-            next_url = "https://scholar.google.com/citations?view_op=view_org&hl=en&org=" + self.institution_id + "&after_author=" + after_author_param + "&astart=" + astart.__str__()
-            url = next_url
-            print("Following Link => " + next_url)
-            for div in profiles:
-                profile_link = div.find('a')['href']
-                profile_name = div.find('a').contents[0]
-                extracted_author_id = self.get_url_param(profile_link, 'user')
-                author_details = scholarly.search_author_id(extracted_author_id)
-                affiliates.append(author_details)
-            i += 1
+        while can_progress:
+            for affiliate_index in trange(10, file=sys.stdout, desc='Progress Report'):
+                page = requests.get(url)
+                soup = BeautifulSoup(page.text, 'html.parser')
+                profiles = soup.find_all("h3", class_="gs_ai_name")
+                try:
+                    next_page = soup.find("button", class_="gsc_pgn_pnx")['onclick']
+                except:
+                    can_progress = False
+                    print("The are no more records to get")
+
+                if next_page is not None:
+                    split_next_page_link = next_page.split('\\')
+                    target_start_author_raw = split_next_page_link[len(split_next_page_link) - 3]
+                    after_author_param = target_start_author_raw.replace('x3d', '')
+                    a_start = i * 10
+                    next_url = "https://scholar.google.com/citations?view_op=view_org&hl=en&org=" + organisation_id + "&after_author=" + after_author_param + "&astart=" + a_start.__str__()
+                    url = next_url
+                    #print("Following Link => " + next_url)
+                    for div in profiles:
+                        profile_link = div.find('a')['href']
+                        profile_name = div.find('a').contents[0]
+                        extracted_author_id = self.get_url_param(profile_link, 'user')
+                        author_details = scholarly.search_author_id(extracted_author_id)
+                        affiliates.append(author_details)
+                    i += 1
 
         return affiliates
